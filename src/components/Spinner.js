c3fac56e8ca5e9c24fa6d4378aa97550aaa8a975
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-08-16 15:14:22 +0530

    Commit

--- src/components/Spinner.js
+++
@@ -1,9 +0,0 @@
-const Spinner = () => {
-  return (
-    <div className='loadingSpinnerContainer' data-testid="spin-container">
-      <div className='loadingSpinner' data-testid="inner-container" />
-    </div>
-  )
-}
-
-export default Spinner
