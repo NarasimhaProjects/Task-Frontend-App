5f3d588478d81cf439f627590454b002d052528d
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-08-23 11:20:14 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import Register from './components/Register';
+import Register from './components/Register'
+import Header from './components/Headers'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import TaskList from './components/TaskList';
 
-function App () {
+function App() {
   return (
-    <Router>
-      <div className='container'>
-        <Routes>
-            <Route path='/' element={<Dashboard />}/>
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/alltasks' element={<TaskList />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
